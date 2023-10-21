dfe7a0d36f25013ebfa9b7dedd4920c4fe6c7878
Souhail M. <souhail.merroun1996@gmail.com>
2023-10-21 10:53:59 +0000

    testing button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main>
       Hello World
+      <Button>Click me</Button>
     </main>
   )
 }
